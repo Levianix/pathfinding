8eb167ef6a04975b76de5b3ac140a97ac467d26b
Diego Alonso Mercado Brizuela <Diego3207>
2021-12-03 12:18:54 -0600

    a

---
+++ path.py
@@ -0,0 +1,32 @@
+matriz=[["A","B","C","D","E"],
+        ["F","G","H","I","J"],
+        ["K","L","M","O","P"],
+        ["Q","R","R","S","T"],
+        ["U","V","W","X","Y"],
+        ]
+
+"""
+for m in matriz:
+    print(m)
+"""
+
+#"""
+lisAb=[]
+lisCe=[]
+for f in range(5):
+    #pisos
+    for c in range(5):
+        print(matriz[f][c])
+
+        #print(matriz[f][c],end=' ')
+    #print()
+
+
+lisAb.append("K")
+
+print(lisAb)
+#i=lisAb.pop(0)
+
+#print("Desencolado: ",i)
+#"""
+
