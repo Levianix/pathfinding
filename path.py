1d1e7d7bc8fb482e84b740251bfce9372d9de546
Levianix <diego3207@yandex.com>
2021-12-03 19:10:02 -0600

    grafo aux

--- path.py
+++ path.py
@@ -1,19 +1,45 @@
+from pprint import pprint
+
+grafoAux={
+    # primer columna
+    "A": ("B","G","F"),
+    "F": ("A","B","G","L","K"),
+    "K": ("F","G","L","R","Q"),
+
+    # segunda columna
+    "B": ("A","F","G","H","C"),
+    "G": ("A","B","C","H","M","L","K","F"),
+    "L": ("K","F","G","H","M"),
+
+    # tercer columna
+    "C": ("B","G","H"),
+    "H": ("C","B","G","L","M"),
+    "M": ("H","G","L")
+}
+
+
 grafo={
+    # primer columna
     "A": ("B","G","F"),
     "F": ("A","B","G","L","K"),
     "K": ("F","G","L","R","Q"),
     "Q": ("K","L","R","V","U"),
-    "U": ("Q","R","V")
+    "U": ("Q","R","V"),
+
+    # segunda columna
+    "B": ("A","F","G","H","C"),
+    "G": ("A","B","C","H","M","L","K","F")
 }
 
+"""
 for g in grafo.values():
     print(g)
-
+"""
 
 l=[]
 for k in grafo["A"]:
-    print(k)
-
     l.append(k)
+    
+#pprint(grafoAux)
 
 print("l: ",l)
